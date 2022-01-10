a8a52ad5cd52c9f28570a575637fbe3396819827
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-10 14:04:33 -0800

    wrote out apps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
+import { getUser, logout } from './services/users';
+import Auth from './Views/Auth/Auth';
+import SignIn from './Views/Auth/SignIn';
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/">
+            {currentUser && (
+              <>
+                <h1>I am signed in</h1>
+                <button onClick={logoutUser}>Log Out</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+            <SignIn />
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>

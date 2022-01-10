f2b2a58e3c76cd13ddf2351330bfdc6b0cef2cc8
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-10 14:35:23 -0800

    wrote out my sign in and pasted to sign up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useState } from 'react';
 import { getUser, logout } from './services/users';
-import Auth from './Views/Auth/Auth';
+// import Auth from './Views/Auth/Auth';
 import SignIn from './Views/Auth/SignIn';
 import './App.css';
 
@@ -15,14 +15,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/">
+          <Route path="/">
             {currentUser && (
               <>
                 <h1>I am signed in</h1>
                 <button onClick={logoutUser}>Log Out</button>
               </>
             )}
-            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+            {!currentUser && <h1>Not signed it</h1>}
             <SignIn />
           </Route>
         </Switch>
---
+++ src/Views/Auth/SignUp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { signUpUser } from '../../services/users';
+import AuthForm from '../../Components/AuthForm/AuthForm';
+
+export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await signUpUser(email, password);
+    } catch {
+      setError('Oh No Something is wrong! Try Again!');
+    }
+  };
+  return (
+    <div>
+      <AuthForm
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        error={error}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
+}
